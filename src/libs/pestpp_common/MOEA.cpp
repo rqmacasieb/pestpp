cd8b467abecccbb0143b1a53832d2bf406894540
rqmacasieb <reygie.macasieb@research.uwa.edu.au>
2024-03-25 09:46:06 +0800

    debugging fitness scaling factor

--- src/libs/pestpp_common/MOEA.cpp
+++ src/libs/pestpp_common/MOEA.cpp
@@ -1013,7 +1013,7 @@ double ParetoObjectives::get_euclidean_fitness(double E, double V)
 	if (beta == 0)
 		val = pow(E / exp(pow(V, 0.5)),0.5);
 	else
-		val = pow(E,0.5);
+		val = pow(E / (beta * pow(V, 0.5) + 1),0.5);
 	
 	return val;
 }
@@ -1069,7 +1069,7 @@ pair<map<string, double>, map<string, double>> ParetoObjectives::get_euclidean_c
 		sortedset crowd_sorted(omap.begin(), omap.end(), compFunctor);
 		sortedset::iterator start = crowd_sorted.begin(), last = prev(crowd_sorted.end(), 1);
 		obj_range[obj_map.first] = last->second - start->second;*/
-		obj_range[obj_map.first] = 1; //just for debugging
+		obj_range[obj_map.first] = 1;
 	}
 
 	for (auto obj_map : obj_member_map)
